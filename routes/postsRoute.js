3ebb91fe079d9d1732479a1339b1c7fcd557f140
AkashP27 <akashphayade27@gmail.com>
2023-07-13 11:06:03 +0530

    reconfigured route

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -7,9 +7,9 @@ router.use(cors());
 router.route("/").post(postsController.postForm);
 router.route("/").get(postsController.getForm);
 router
- .route(":id")
- .get(postsController.getForm)
- .put(postsController.updateForm)
- .delete(postsController.deleteForm);
+	.route("/:id")
+	.get(postsController.getForm)
+	.put(postsController.updateForm)
+	.delete(postsController.deleteForm);
 
 module.exports = router;
