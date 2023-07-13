3be662d3518eefd292620a492c037ed2bd84fe99
AkashP27 <akashphayade27@gmail.com>
2023-07-13 11:09:15 +0530

    cloudinary preset

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -4,9 +4,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 cloudinary.config({
- cloud_name: process.env.CLOUD_NAME,
- api_key: process.env.CLOUD_API_KEY,
- api_secret: process.env.CLOUD_API_SECRET,
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUD_API_KEY,
+	api_secret: process.env.CLOUD_API_SECRET,
 });
+
+cloudinary.api.create_upload_preset({
+	name: "mern",
+	folder: "blog",
+});
+// .then((result) => console.log(result))
+// .catch((err) => console.error(err));
 
 module.exports = cloudinary;
