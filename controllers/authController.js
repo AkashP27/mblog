02aacd254a0856c2b3bdeee1c5f5a928591fe7ff
AkashP27 <akashphayade27@gmail.com>
2023-07-21 12:37:23 +0530

    creating jwt for loggedIn user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,37 +1,54 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-exports.registerForm = async (req, res) => {
- try {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPass = await bcrypt.hash(req.body.password, salt);
-  // const hashedConfPass = await bcrypt.hash(req.body.confpassword, salt);
-
-  const newUser = new User({
-   name: req.body.name,
-   email: req.body.email,
-   password: hashedPass,
-   // confpassword: hashedConfPass,
-  });
-
-  const user = await newUser.save();
-  res.status(200).json(user);
- } catch (err) {
-  res.status(500).json(err);
- }
+const signToken = (id, name) => {
+	return jwt.sign({ id, name }, process.env.JWT_SECRET_KEY, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
 };
 
-exports.loginForm = async (req, res) => {
- try {
-  const user = await User.findOne({ name: req.body.name });
-  !user && res.status(400).json("Wrong credentials");
+exports.registerForm = async (req, res) => {
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPass = await bcrypt.hash(req.body.password, salt);
+		// const hashedConfPass = await bcrypt.hash(req.body.confpassword, salt);
 
-  const validated = await bcrypt.compare(req.body.password, user.password);
-  !validated && res.status(400).json("Wrong credentials");
+		const newUser = new User({
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPass,
+			// confpassword: hashedConfPass,
+		});
 
-  const { password, ...others } = user._doc;
-  res.status(200).json(others);
- } catch (err) {
-  res.status(500).json(err);
- }
+		const user = await newUser.save();
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
+
+exports.loginUser = async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+
+		if (!user) {
+			return res.status(400).json("Wrong credentials");
+		}
+
+		const validated = await bcrypt.compare(req.body.password, user.password);
+
+		if (!validated) {
+			return res.status(400).json("Wrong credentials");
+		}
+
+		const token = signToken(user._id, user.name);
+		// console.log(user._docs);
+
+		const { password, ...others } = user._doc;
+		res.status(200).json({ ...others, token });
+	} catch (err) {
+		// res.status(500).json(err);
+		console.log(err);
+	}
 };
