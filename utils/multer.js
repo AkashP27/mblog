36f1308e9262d6b635e17ab431252b20c443fa7b
AkashP27 <akashphayade27@gmail.com>
2023-09-29 10:50:14 +0530

    image format specified with error handling

--- utils/multer.js
+++ utils/multer.js
@@ -1,15 +1,22 @@
 const multer = require("multer");
 const path = require("path");
+const AppError = require("../utils/appError");
 
 // Multer config
 module.exports = multer({
- storage: multer.diskStorage({}),
- fileFilter: (req, file, cb) => {
-  let ext = path.extname(file.originalname);
-  if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-   cb(new Error("File type is not supported"), false);
-   return;
-  }
-  cb(null, true);
- },
+	storage: multer.diskStorage({}),
+	fileFilter: (req, file, cb) => {
+		let ext = path.extname(file.originalname);
+		if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+			cb(
+				new AppError(
+					"File type is not supported. Only png jpg jpeg is allowed",
+					415
+				),
+				false
+			);
+			return;
+		}
+		cb(null, true);
+	},
 });
