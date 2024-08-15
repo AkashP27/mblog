1aa66454aa5ee483abe56b1c0aedb3f3602645be
AkashP27 <akashphayade27@gmail.com>
2024-08-15 10:40:31 +0530

    customized navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,70 +1,189 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect, useRef } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
 import "../styles/navbar.css";
 import "../help.css";
 import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "../store/auth-slice";
+import useProgressBar from "../custom-hooks/useProgressBar";
+import jwt_decode from "jwt-decode";
+import Avatar from "react-avatar";
 
 const Navbar = () => {
 	const token = useSelector((state) => state.authentication.token);
+	const avatarURL = useSelector((state) => state.authentication.avatarURL);
+	if (token) {
+		var decoded = jwt_decode(token);
+	}
+
 	const dispatch = useDispatch();
+	const [activeLink, setActiveLink] = useState("/");
 	const [menuOpen, setMenuOpen] = useState(false);
-	let menuRef = useRef();
+	const [profileOpen, setProfileOpen] = useState(false);
+	const profileRef = useRef(null);
+
+	const progress = useProgressBar();
 
 	useEffect(() => {
-		let handler = (e) => {
-			if (!menuRef.current.contains(e.target)) {
-				setMenuOpen(false);
+		const storedActiveLink = localStorage.getItem("activeLink");
+		setActiveLink(storedActiveLink || "/");
+	}, [activeLink]);
+
+	useEffect(() => {
+		if (menuOpen) {
+			document.body.style.overflowY = "hidden";
+			document.body.addEventListener("click", handleBodyClick);
+		} else {
+			document.body.style.overflowY = "scroll";
+			document.body.removeEventListener("click", handleBodyClick);
+		}
+
+		return () => {
+			document.body.removeEventListener("click", handleBodyClick);
+		};
+	}, [menuOpen]);
+
+	useEffect(() => {
+		const handleOutsideClick = (event) => {
+			if (profileRef.current && !profileRef.current.contains(event.target)) {
+				setProfileOpen(false);
 			}
 		};
-		document.addEventListener("mousedown", handler);
-	});
+
+		document.addEventListener("mousedown", handleOutsideClick);
+		return () => {
+			document.removeEventListener("mousedown", handleOutsideClick);
+		};
+	}, []);
 
 	const handleLogout = () => {
 		dispatch(authActions.logout());
+		setActiveLink("/");
+		localStorage.setItem("activeLink", "/");
+	};
+
+	const handleLinkClick = (link) => {
+		setActiveLink(link);
+		localStorage.setItem("activeLink", link);
+		setMenuOpen(false);
+	};
+
+	const handleBodyClick = (event) => {
+		if (!event.target.closest(".nav-menu")) {
+			setMenuOpen(false);
+		}
 	};
 
 	return (
 		<>
-			<nav className="main_nav max_width m_auto">
-				<NavLink to="/" className="logo">
-					MBlog
-				</NavLink>
-
-				<div ref={menuRef}>
-					<ul className={menuOpen ? "nav-menu active" : "nav-menu"}>
-						<li className="nav-item">
-							<NavLink to="/">Home</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink to="/myprofile">MyProfile</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink className=" create-btnnnn" to="/write">
-								Create
-							</NavLink>
-						</li>
-						<li className="nav-item" onClick={handleLogout}>
+			<nav>
+				<div className="main-nav">
+					<div className="nav-bar max_width m_auto">
+						<NavLink
+							to="/"
+							className="logo"
+							onClick={() => handleLinkClick("/")}
+						>
+							<img src="/logo.svg" alt="" />
+						</NavLink>
+						<ul className={menuOpen ? "nav-menu active" : "nav-menu"}>
+							<li>
+								<NavLink
+									to="/"
+									activeClassName={activeLink === "/" ? "active" : ""}
+									onClick={() => handleLinkClick("/")}
+								>
+									Home
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									to="/about"
+									activeClassName={activeLink === "/about" ? "active" : ""}
+									onClick={() => handleLinkClick("/about")}
+								>
+									About
+								</NavLink>
+							</li>
 							{token ? (
-								<NavLink className="rightt" to="/" style={{ color: "#fa7575" }}>
-									Logout
-								</NavLink>
+								<>
+									<li>
+										<NavLink
+											to="/write"
+											activeClassName={activeLink === "/write" ? "active" : ""}
+											onClick={() => handleLinkClick("/write")}
+										>
+											Create
+										</NavLink>
+									</li>
+								</>
 							) : (
-								<NavLink className="rightt" to="/login">
-									Login
-								</NavLink>
+								<li>
+									<NavLink
+										className="login-btn"
+										to="/login"
+										onClick={() => handleLinkClick("/login")}
+									>
+										Login
+									</NavLink>
+								</li>
 							)}
-						</li>
-					</ul>
+						</ul>
+						{token && (
+							<div
+								className={profileOpen ? "user active" : "user"}
+								onClick={() => setProfileOpen(!profileOpen)}
+								ref={profileRef}
+							>
+								<span className="profile_pic">
+									<Avatar
+										name={decoded.name}
+										src={avatarURL ? avatarURL : ""}
+										size={34}
+										round={true}
+										maxInitials={3}
+										className="myprofile_img"
+									/>
+								</span>
+								<ul className="dropdown-menu">
+									<div className="user-name">
+										<span className="profile_pic">
+											<Avatar
+												name={decoded.name}
+												src={avatarURL ? avatarURL : ""}
+												size={34}
+												round={true}
+												maxInitials={3}
+												className="myprofile_img"
+											/>
+										</span>
+										<strong>@{decoded.name}</strong>
+									</div>
+									<li>
+										<Link to="/myprofile">
+											<i class="nav_icons fa fa-user"></i>
+											Profile
+										</Link>
+									</li>
+									<li>
+										<NavLink to="/" onClick={handleLogout} className="logout">
+											<i class="nav_icons fa fa-sign-out"></i>
+											Logout
+										</NavLink>
+									</li>
+								</ul>
+							</div>
+						)}
+						<div id="mobile" onClick={() => setMenuOpen(!menuOpen)}>
+							<i className={menuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+						</div>
+					</div>
 				</div>
 
-				<div id="mobile" onClick={() => setMenuOpen(!menuOpen)}>
-					{/* <i className={menuOpen ? "fas fa-times" : "fas fa-bars"}></i> */}
-					<i className="fas fa-bars"></i>
-				</div>
+				<span
+					style={{ transform: `translate(${progress - 100}%)` }}
+					className="progress-scrollbar"
+				/>
 			</nav>
-			<hr className="max_width m_auto" />
 		</>
 	);
 };
