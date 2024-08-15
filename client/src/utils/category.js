9a9595b6f2300cd074e10e1c22683d1610d3d516
AkashP27 <akashphayade27@gmail.com>
2024-08-15 14:04:55 +0530

    category utilities

---
+++ client/src/utils/category.js
@@ -0,0 +1,34 @@
+import Style from "../images/style.jpg";
+import Fitness from "../images/fitness.jpg";
+import Finance from "../images/finance.jpg";
+import Fashion from "../images/fashion.jpg";
+import Food from "../images/food.jpg";
+import Culture from "../images/culture.jpg";
+import Technology from "../images/technology.jpg";
+import Gaming from "../images/gaming.jpg";
+import Coding from "../images/coding.jpg";
+import Travel from "../images/travel.jpg";
+import Pets from "../images/pets.jpg";
+import Animals from "../images/animals.jpg";
+import Politics from "../images/politics.jpg";
+import Education from "../images/education.jpg";
+
+const categories = [
+	{ value: "", label: "All", image: Education },
+	{ value: "Style", label: "Style", image: Style },
+	{ value: "Fitness", label: "Fitness", image: Fitness },
+	{ value: "Finance", label: "Finance", image: Finance },
+	{ value: "Fashion", label: "Fashion", image: Fashion },
+	{ value: "Food", label: "Food", image: Food },
+	{ value: "Culture", label: "Culture", image: Culture },
+	{ value: "Technology", label: "Technology", image: Technology },
+	{ value: "Gaming", label: "Gaming", image: Gaming },
+	{ value: "Coding", label: "Coding", image: Coding },
+	{ value: "Travel", label: "Travel", image: Travel },
+	{ value: "Pets", label: "Pets", image: Pets },
+	{ value: "Animals", label: "Animals", image: Animals },
+	{ value: "Politics", label: "Politics", image: Politics },
+	{ value: "Education", label: "Education", image: Education },
+];
+
+export default categories;
---
+++ client/src/utils/categoryColors.js
@@ -0,0 +1,19 @@
+const categoryColors = {
+	Style: "#57c4ff31",
+	Fitness: "#da85c731",
+	Finance: "#7fb88133",
+	Fashion: "#ff795736",
+	Food: "#ffb04f45",
+	Culture: "#5e4fff31",
+	Technology: "#57c4ff31",
+	Gaming: "#da85c731",
+	Coding: "#7fb88133",
+	Travel: "#ff795736",
+	Pets: "#ffb04f45",
+	Animals: "#5e4fff31",
+	Politics: "#57c4ff31",
+	Education: "#da85c731",
+	Default: "#cccccc",
+};
+
+export default categoryColors;
