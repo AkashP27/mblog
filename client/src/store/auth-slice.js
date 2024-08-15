15d83365a5a032c4d09899d89c00cc8ee39d4204
AkashP27 <akashphayade27@gmail.com>
2024-08-15 15:26:38 +0530

    storing avatarURL in localstorage

--- client/src/store/auth-slice.js
+++ client/src/store/auth-slice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authInitialState = {
 	token: JSON.parse(localStorage.getItem("token")) || null,
+	avatarURL: JSON.parse(localStorage.getItem("avatarURL")) || "",
 	isFetching: false,
 	error: false,
 };
@@ -12,30 +13,38 @@ const authSlice = createSlice({
 	reducers: {
 		loginStart: (state) => {
 			state.token = null;
+			state.avatarURL = "";
 			state.isFetching = true;
 			state.error = false;
 			localStorage.setItem("token", JSON.stringify(state.token));
+			localStorage.setItem("avatarURL", JSON.stringify(state.avatarURL));
 		},
 
 		loginSuccess: (state, action) => {
 			state.token = action.payload.token;
+			state.avatarURL = action.payload.user.avatarURL;
 			state.isFetching = false;
 			state.error = false;
 			localStorage.setItem("token", JSON.stringify(state.token));
+			localStorage.setItem("avatarURL", JSON.stringify(state.avatarURL));
 		},
 
 		loginFailure: (state) => {
 			state.token = null;
+			state.avatarURL = "";
 			state.isFetching = false;
 			state.error = true;
 			localStorage.setItem("token", JSON.stringify(state.token));
+			localStorage.setItem("avatarURL", JSON.stringify(state.avatarURL));
 		},
 
 		logout: (state) => {
 			state.token = null;
+			state.avatarURL = "";
 			state.isFetching = false;
 			state.error = false;
 			localStorage.setItem("token", JSON.stringify(state.token));
+			localStorage.setItem("avatarURL", JSON.stringify(state.avatarURL));
 		},
 	},
 });
