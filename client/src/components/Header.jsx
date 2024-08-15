4fac30ddd8c39e1506c5275a23b0cb68e730740e
AkashP27 <akashphayade27@gmail.com>
2024-08-15 10:57:11 +0530

    changed header UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,39 +1,39 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import main from "../images/main1.svg";
 import "../styles/header.css";
 
 const Header = () => {
+	const token = useSelector((state) => state.authentication.token);
 	return (
-		<>
-			<br />
-			<br />
+		<div className="head_wrapper ">
 			<div className="head_sec max_width m_auto">
 				<div className="heading_left">
-					<h1>Multipurpose Blog</h1>
-					<br />
-					<p>
-						MBlog is a platform where an individual can share their experience,
-						knowledge, interest at free of cost. MBlog offers a secure and
-						reliable way to create a blog who wants to start blogging as a hobby
-						or are already serious about it. In the present time where people
-						have access to anything and everything with just clicking a button,
-						MBlog provides a way where people can connect. Go on start blogging
-						by registering yourself...!
+					<p className="animate">
+						Unleash <br /> Your <br />
+						Creativity
 					</p>
+					<span className="animate">
+						Multipurpose Blog, <br /> Your Canvas for Captivating Stories..!
+					</span>
+					<div>
+						{token ? (
+							<NavLink to="/write" className="header_signup animate">
+								create post
+							</NavLink>
+						) : (
+							<NavLink to="/register" className="header_signup animate">
+								sign up
+							</NavLink>
+						)}
+					</div>
 				</div>
-				<div className="heading_right">
-					<img src={main} alt="" />
+				<div className="heading_right animate">
+					<img src={main} alt="Creative blog illustration" />
 				</div>
 			</div>
-
-			<br />
-			<br />
-			<hr className="max_width m_auto" />
-
-			<div className="max_width m_auto">
-				<br />
-			</div>
-		</>
+		</div>
 	);
 };
 
